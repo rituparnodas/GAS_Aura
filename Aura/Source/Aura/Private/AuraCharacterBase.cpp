fbf67e9141fb1c4e58033ed4663388b6ecd00a8e
Rituparno Das <rituparnodas21@gmail.com>
2024-08-30 20:05:45 +0530

    Added Project Files!!!

---
+++ Aura/Source/Aura/Private/AuraCharacterBase.cpp
@@ -0,0 +1,23 @@
+// Copyright © 2024 Global Grand Gaming Private Limited
+
+#include "AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+UAbilitySystemComponent* AAuraCharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
